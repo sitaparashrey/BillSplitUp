43322895c85ca08aba64f92e33330ceef1162f5b
SHREY <61205409+shrey987@users.noreply.github.com>
2022-08-07 22:46:31 +0530

    Update Homeheader.js

--- src/Homeheader.js
+++ src/Homeheader.js
@@ -1,7 +1,7 @@
 import {Button, Container,Nav,Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import logo from './plit-up.svg';
+import logo from './Split-up.svg';
 
 function Header()
 {
